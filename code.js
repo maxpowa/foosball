10cbdc01c5408f5c45c41fb6f13d3e7824a4f24b
ponglordzGithub <ponglordz@gmail.com>
2013-09-02 18:34:45 -0400

    Update code.js

--- code.js
+++ code.js
@@ -218,7 +218,7 @@ $(document).ready(function() {
     $('body').on('click', '.team1score,.team2score', function(e) {
         e.stopImmediatePropagation();
         $(this).button('toggle');
-        $($(this).hasClass('team1score') ? '#score25' : '#score15').button('toggle');
+        $($(this).hasClass('team1score') ? '#score210' : '#score110').button('toggle');
         validate();
     });
 
